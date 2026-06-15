ed1516ab6fbcb88af6e76167e7f10603899448f0
Armaan Sharma <armaan2345sharma@gmail.com>
2026-06-15 15:52:59 +0000

    learning enumerate function

---
+++ CW1/09_inumrate.py
@@ -0,0 +1,8 @@
+lis1={1,"nam","aru",4,5}
+index=0
+#for i in enumerate(lis1):
+    #print(i)
+    #index+=1
+    #print(f"Index: {index}")
+for index,i in enumerate (lis1):#enumerate return a tuple of index and vales 
+    print(f" {index}, Value: {i}") 
