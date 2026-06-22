b000b3997940e504a0ed4073ca459ab0dd4ccd97
Armaan Sharma <armaan2345sharma@gmail.com>
2026-06-22 05:41:01 +0000

    learning lambda function and join func in python

---
+++ CW2/02_lamda.py
@@ -0,0 +1,11 @@
+#def func(a):
+   # return a+5
+func = lambda a: a + 5
+squ = lambda a: a*a
+add = lambda a,b,c : a+b+c
+
+
+a = 556
+print(func(a))
+print(squ(a))
+print(add(a, 10, 20))
---
+++ CW2/03_join.py
@@ -0,0 +1,4 @@
+l=["graphic card","comp","ram","ssd"]
+sentence = "\n".join(l)
+print(sentence)
+print(type(sentence))
